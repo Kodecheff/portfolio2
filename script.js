f38e64238884dbc5b883d5bef168906fe55cd526
kodecheff <akunneemeka125@gmail.com>
2020-08-18 00:33:21 +0100

    fix dropdown menu

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+
 let links = document.querySelectorAll('nav ul a');
 
 window.addEventListener('scroll', event => {
@@ -14,5 +14,23 @@ window.addEventListener('scroll', event => {
     }
   })
 })
+
+
+function dropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
 
