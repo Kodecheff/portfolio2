5eb0d8208ec0df43de154fa1e15d1be0767086ca
kodecheff <akunneemeka125@gmail.com>
2021-03-02 00:00:32 +0100

    added blog page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let links = document.querySelectorAll('nav ul a');
+let links = document.querySelectorAll('.hashLink');
 let showMoreBtn = document.querySelector('#showMore')
 let otherWorks = document.querySelectorAll('.otherWorks')
 
@@ -43,4 +43,64 @@ function expand() {
   })
 
   showMoreBtn.textContent == "Show more" ? showMoreBtn.textContent = "Show less" : showMoreBtn.textContent = "Show more"
+}
+
+
+
+
+// Script for blog.html
+
+let blogPosts = document.querySelector('.blogPosts')
+
+const query = `{
+  user(username: "kodecheff"){
+    publication {
+      posts{
+        slug
+        title
+        brief
+        coverImage
+      }
+    }
+  }
+}`
+
+let state = {
+  post: []
+}
+
+// Fetches post via hashnode
+const fetchPosts = async () => {
+  const response = await fetch('https://api.hashnode.com', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({query})
+  })
+
+  const ApiResponse = await response.json()
+
+  state.post = ApiResponse.data.user.publication.posts
+
+  state.post.map((post, index) => {
+    let anchor = document.createElement('a')
+    Element.prototype.setAttributes = function(obj){ for(var prop in obj) this.setAttribute(prop, obj[prop]) }
+    anchor.setAttributes({'href': `https://kodecheff.hashnode.dev/${post.slug}`, 'target': '_blank'})
+    let div = document.createElement('div')
+    let img = document.createElement('img')
+    let h2 = document.createElement('h2')
+    let p = document.createElement('p')
+
+    img.setAttributes({'src': `${post.coverImage}`, 'alt': `${post.title}`, 'class': 'img-fluid'})
+    h2.textContent = `${post.title}`
+    p.textContent = `${post.brief}`
+
+    div.append(img, h2, p)
+
+    anchor.appendChild(div)
+
+    blogPosts.appendChild(anchor)
+  })
+  console.log(ApiResponse.data.user.publication.posts)
 }
